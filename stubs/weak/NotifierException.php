334dc8f4b9d44c42e6af094823fcf3339edc5096
Bogdan Padalko <pinepain@gmail.com>
2016-07-14 22:26:35 +0300

    Add missed license note to stubs [skip ci]

--- stubs/weak/NotifierException.php
+++ stubs/weak/NotifierException.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the pinepain/php-weak PHP extension.
+ *
+ * Copyright (c) 2016 Bogdan Padalko <zaq178miami@gmail.com>
+ *
+ * Licensed under the MIT license: http://opensource.org/licenses/MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit http://opensource.org/licenses/MIT
+ */
+
 
 namespace Weak;
 
