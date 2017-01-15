ba7ea2198717cdd4b2c06d2d1fecb78c7ce36301
Bogdan Padalko <pinepain@gmail.com>
2017-01-16 00:08:56 +0200

    Unify copyright headers and bump year

--- php_ref_reference.h
+++ php_ref_reference.h
@@ -1,16 +1,14 @@
 /*
-  +----------------------------------------------------------------------+
-  | This file is part of the pinepain/php-ref PHP extension.             |
-  |                                                                      |
-  | Copyright (c) 2016 Bogdan Padalko <pinepain@gmail.com>               |
-  |                                                                      |
-  | Licensed under the MIT license: http://opensource.org/licenses/MIT   |
-  |                                                                      |
-  | For the full copyright and license information, please view the      |
-  | LICENSE file that was distributed with this source or visit          |
-  | http://opensource.org/licenses/MIT                                   |
-  +----------------------------------------------------------------------+
-*/
+ * This file is part of the pinepain/php-ref PHP extension.
+ *
+ * Copyright (c) 2016-2017 Bogdan Padalko <pinepain@gmail.com>
+ *
+ * Licensed under the MIT license: http://opensource.org/licenses/MIT
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source or visit
+ * http://opensource.org/licenses/MIT
+ */
 
 #ifndef PHP_REF_REFERENCE_H
 #define PHP_REF_REFERENCE_H
