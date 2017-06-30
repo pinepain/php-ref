5240d14fa32c12cbcedd594c635f23e2f7451339
Bogdan Padalko <pinepain@gmail.com>
2017-06-30 20:56:39 +0300

    Prepare 0.4.3 release

--- php_ref.h
+++ php_ref.h
@@ -19,11 +19,11 @@ extern zend_module_entry php_ref_module_entry;
 #define phpext_ref_ptr &php_ref_module_entry
 
 #ifndef PHP_REF_VERSION
-#define PHP_REF_VERSION "0.5.0"
+#define PHP_REF_VERSION "0.4.3"
 #endif
 
 #ifndef PHP_REF_REVISION
-#define PHP_REF_REVISION "dev"
+#define PHP_REF_REVISION "release"
 #endif
 
 #if PHP_VERSION_ID < 70003
